c8037c1190e58e9a4e49795e898516c106704765
Russel <russelcuevas0@gmail.com>
2025-04-07 20:29:22 +0800

    change-passokay

--- admin/components/right_sidebar.php
+++ admin/components/right_sidebar.php
@@ -28,6 +28,37 @@
     </div>
 </aside>
 
+
+<!-- CHANGE PASSWORD EXECUTE -->
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (!empty($_POST['new_password']) && !empty($_POST['confirm_password'])) {
+        $new_password = $_POST['new_password'];
+        $confirm_password = $_POST['confirm_password'];
+        if ($new_password === $confirm_password) {
+            $hashed_password = sha1($new_password);
+
+            $admin_id = $_SESSION['admin_id'];
+
+            $query = "UPDATE tbl_admin SET password = :password WHERE id = :admin_id";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(':password', $hashed_password, PDO::PARAM_STR);
+            $stmt->bindParam(':admin_id', $admin_id, PDO::PARAM_INT);
+
+            if ($stmt->execute()) {
+                $_SESSION['success'] = "Password successfully updated!";
+            } else {
+                $_SESSION['errors'] = "There was an error updating the password.";
+            }
+        } else {
+            $_SESSION['errors'] = "The passwords do not match!";
+        }
+    } else {
+        $_SESSION['errors'] = "Please fill in both the new password and confirm password fields.";
+    }
+}
+?>
+
 <div class="modal fade" id="changePassModal" tabindex="-1" role="dialog" style="display: none;">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -35,25 +66,25 @@
                 <h4 class="modal-title" id="defaultModalLabel">Change Password</h4>
             </div>
             <div class="modal-body">
-                <form id="form_advanced_validation" style="margin-top:10px;" method="POST">
+                <form id="form_advanced_validation" method="POST" style="margin-top:10px;">
                     <div class="form-group form-float">
                         <div class="form-line">
-                            <input type="text" class="form-control" name="new_password" maxlength="10" minlength="3" required>
+                            <input type="password" class="form-control" name="new_password" maxlength="10" minlength="3" required>
                             <label class="form-label">New Password</label>
                         </div>
                     </div>
                     <div class="form-group form-float">
                         <div class="form-line">
-                            <input type="text" class="form-control" name="confirm_password" maxlength="10" minlength="3" required>
+                            <input type="password" class="form-control" name="confirm_password" maxlength="10" minlength="3" required>
                             <label class="form-label">Confirm Password</label>
                         </div>
                     </div>
+                    <div class="modal-footer">
+                        <button class="btn bg-teal waves-effect" type="submit">SAVE</button>
+                        <button type="button" class="btn btn-link waves-effect" data-dismiss="modal">CLOSE</button>
+                    </div>
+                </form>
             </div>
-            <div class="modal-footer">
-                <button class="btn bg-teal waves-effect" type="submit">SAVE</button>
-                <button type="button" class="btn btn-link waves-effect" data-dismiss="modal">CLOSE</button>
-            </div>
-            </form>
         </div>
     </div>
 </div>
--- admin/examinees_list.php
+++ admin/examinees_list.php
@@ -1,3 +1,8 @@
+<?php
+include '../database/connection.php';
+include 'session_not_login.php';
+?>
+
 <!DOCTYPE html>
 <html>
 
